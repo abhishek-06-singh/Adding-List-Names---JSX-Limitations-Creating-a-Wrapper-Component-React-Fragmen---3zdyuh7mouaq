db374682875daea7945b924d0cfd403143588486
Abhishek singh chauahn <115978151+abhishek-06-singh@users.noreply.github.com>
2023-01-07 21:08:11 +0530

    solved

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,27 @@
-import React , {useState,useEffect} from 'react'
-import '../styles/App.css';
+import React, { useState, useEffect } from "react";
+import "../styles/App.css";
 import List from "./List";
 const App = () => {
   //code here
+  const [value, setValue] = useState(0);
+  const [list, setList] = useState([]);
+  const handleOnChange = (e) => {
+    setValue(e.target.value);
+  };
+  const handleOnClick = () => {
+    setList((prev) => [...prev, value]);
+  };
   return (
     <div id="main">
-       <input id="input" onChange={} />
-      <button id="button" onClick={}>Click</button>
-       <ul id="list">
-      <List listx={list} />
+      <input id="input" onChange={handleOnChange} />
+      <button id="button" onClick={handleOnClick}>
+        Click
+      </button>
+      <ul id="list">
+        <List listx={list} />
       </ul>
     </div>
-  )
-}
-
+  );
+};
 
 export default App;
--- src/components/List.js
+++ src/components/List.js
@@ -1,14 +1,14 @@
-import React from 'react';
-import ListItems from './ListItems';
+import React from "react";
+import ListItems from "./ListItems";
 //code here
- const List = ({listx}) =>{
-  return(
-    
- 
-    <ListItems valuex={i} />
-    
-  
-  )
-}
+const List = ({ listx }) => {
+  return (
+    <React.Fragment>
+      {listx.map((i) => {
+        return <ListItems valuex={i} />;
+      })}
+    </React.Fragment>
+  );
+};
 
 export default List;
